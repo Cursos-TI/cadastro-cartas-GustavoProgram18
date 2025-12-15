c06cd866522ab8b25e5dd6bf2e810e6fa17ad61a
GustavoProgram18 <gustavodasilva01112003@gmail.com>
2025-12-15 15:16:17 +0000

    termino de area para entrada de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,24 +8,37 @@ int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 char estado1;
 int cidade1;
-char nomeCidade[60];
+char nomeCidade1[60];
 long int populacao1;
 float area1;
 float pib1;
-int pontosturisticos1;
+int pontosTuristicos1;
 
   // Área para entrada de dados
 
 printf("=== Cadastro da Carta 1 ===\n");
 
-printf(" A ");
+printf("Digite a letra do estado (A a H): ");
 scanf(" %c", &estado1);
 
-printf(" 1 ");
-scanf(" %d", &cidade1);
+printf("Digite o numero da cidade (1 a 4): ");
+scanf("%d", &cidade1);
 
-printf(" Santo Domingo - Republica Dominicana ");
+printf("Digite o nome da cidade: ");
 scanf(" %59[^\n]", nomeCidade1);
+
+printf("Digite a populacao: ");
+scanf("%ld", &populacao1);
+
+printf("Digite a area (ex: 123.45): ");
+scanf("%f", &area1);
+
+printf("Digite o PIB (ex: 987.65): ");
+scanf("%f", &pib1);
+
+printf("Digite o numero de pontos turisticos: ");
+scanf("%d", &pontosTuristicos1);
+ 
   // Área para exibição dos dados da cidade
 
 return 0;
