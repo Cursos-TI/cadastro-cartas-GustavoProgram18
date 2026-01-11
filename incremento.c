29d4ed629050111c7724ac9fe272777274cb7da0
GustavoProgram18 <gustavodasilva01112003@gmail.com>
2026-01-11 18:25:09 +0000

    pos e pre incremento

--- incremento.c
+++ incremento.c
@@ -15,15 +15,20 @@ int main() {
 
     //numero1 = numero1 + 1;
     //numero1 += 1;
+    //Pos incremento:
+    //resultado = numero1;
+    //numero1++;    
+    resultado = numero1++;
+    //printf("Apos incremento: %d\n", numero1);
+    printf("Apos Pos-incremento - Numero 1: %d - resultado: %d\n", numero1, resultado);
 
     resultado = ++numero1;
-    //printf("Apos incremento: %d\n", numero1);
-    printf("Apos incremento - Numero 1: %d - resultado: %d\n", numero1, resultado);
+    printf("Apos Pre- incremento - Numero 1: %d - resultado: %d\n", numero1, resultado);
 
     //numero1 = numero1 - 1;
     //numero1 -= 1;
 
-    numero1--;
+    //numero1--;
     printf("Apos decremento: %d\n", numero1);
 
     return 0;
